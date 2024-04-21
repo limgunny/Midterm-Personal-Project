4eb2f4a8e7eb11a24b4b71d48daa43a0ea0c2b29
limgunny <red373712345@gmail.com>
2024-04-21 22:57:18 +0900

    as

--- main.js
+++ main.js
@@ -13,14 +13,19 @@ function reset(){
     document.body.style.backgroundColor='white'
 }
 
-function showhtml(){
-    document.getElementById('fig').src ='img/zzanggu.gif'
+function showsecon(){
+    document.getElementById('fig').src ='img/secon.jpg'
     document.getElementById('desc').innerHTML=
-    '<b>짱구테마</b> 입니다.'
+    '<b>세계 보안 엑스포</b>에 참가하여 다양한 기업들이 적용한 보안 솔루션페어에 참관했습니다.'
 }
 
-function showcss(){
-    document.getElementById('fig').src ='img/gif2.gif'
+function showcircuit(){
+    document.getElementById('fig').src ='circuit/speaker.jpg'
     document.getElementById('desc').innerHTML=
-    '<b>삐앙이테마</b> 입니다.'
+    '다양한 <b>전기전자회로실습</b>을 통해서 회로를 해석하고 결선하는 능력을 향상시켰습니다.'
+}
+function showdigital(){
+    document.getElementById('fig').src ='img/digital.png'
+    document.getElementById('desc').innerHTML=
+    '다양한 <b>디지털회로실습</b>을 통해서 하드웨어적인 이진 연산 회로에 대해서 공부하고 회로를 결선하는 능력을 향상시켰습니다.'
 }
